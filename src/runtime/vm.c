368aee7d5ece20f93e6725fcdda298dddb7c4651
Prog. Kanishk Raj <programmerkr.123@gmail.com>
2025-12-16 00:03:16 +0530

    Refactor VM implementation with error handling

--- src/runtime/vm.c
+++ src/runtime/vm.c
@@ -1,44 +1,118 @@
+/* src/runtime/vm.c */
+#include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
 
-#include "common.h"
-#include "debug.h"
-#include "vm.h"
+#include "../include/common.h"
+#include "../include/compiler.h"
+#include "../include/debug.h"
+#include "../include/object.h"
+#include "../include/memory.h"
+#include "../include/vm.h"
+#include "../include/table.h"
 
-void initVM(VM *vm) { vm->stackTop = vm->stack; }
-
-void freeVM(VM *vm) {
-  // Nothing to free yet
+// Helper to reset the stack
+static void resetStack(VM* vm) {
+  vm->stackTop = vm->stack;
 }
 
-void push(VM *vm, Value value) {
+// Runtime Error Helper
+static void runtimeError(VM* vm, const char* format, ...) {
+  va_list args;
+  va_start(args, format);
+  vfprintf(stderr, format, args);
+  va_end(args);
+  fputs("\n", stderr);
+
+  size_t instruction = vm->ip - vm->chunk->code - 1;
+  int line = vm->chunk->lines[instruction];
+  fprintf(stderr, "[line %d] in script\n", line);
+  resetStack(vm);
+}
+
+// Initialize the VM
+void initVM(VM* vm) {
+  resetStack(vm);
+  vm->objects = NULL;
+  initTable(&vm->globals);
+  initTable(&vm->strings);
+  
+  // You can register native functions here if you want them globally available
+  // defineNative(vm, "clock", clockNative); 
+}
+
+// Free the VM resources
+void freeVM(VM* vm) {
+  freeTable(&vm->globals);
+  freeTable(&vm->strings);
+  freeObjects(); // Frees the linked list of objects
+}
+
+void push(VM* vm, Value value) {
   *vm->stackTop = value;
   vm->stackTop++;
 }
 
-Value pop(VM *vm) {
+Value pop(VM* vm) {
   vm->stackTop--;
   return *vm->stackTop;
 }
 
-static InterpretResult run(VM *vm) {
+static Value peek(VM* vm, int distance) {
+  return vm->stackTop[-1 - distance];
+}
+
+// Helper to check for false/nil
+static bool isFalsey(Value value) {
+  return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
+// Concatenate two strings
+static void concatenate(VM* vm) {
+  ObjString* b = AS_STRING(peek(vm, 0));
+  ObjString* a = AS_STRING(peek(vm, 1));
+
+  int length = a->length + b->length;
+  char* chars = ALLOCATE(char, length + 1);
+  memcpy(chars, a->chars, a->length);
+  memcpy(chars + a->length, b->chars, b->length);
+  chars[length] = '\0';
+
+  ObjString* result = takeString(chars, length);
+  pop(vm);
+  pop(vm);
+  push(vm, OBJ_VAL(result));
+}
+
+// Define a native function
+void defineNative(VM* vm, const char* name, NativeFn function) {
+  push(vm, OBJ_VAL(copyString(name, (int)strlen(name))));
+  push(vm, OBJ_VAL(newNative(function)));
+  tableSet(&vm->globals, AS_STRING(vm->stack[0]), vm->stack[1]);
+  pop(vm);
+  pop(vm);
+}
+
+// Main execution loop
+static InterpretResult run(VM* vm) {
 #define READ_BYTE() (*vm->ip++)
 #define READ_CONSTANT() (vm->chunk->constants.values[READ_BYTE()])
-#define BINARY_OP(valueType, op)                                               \
-  do {                                                                         \
-    if (!IS_NUMBER((*(vm->stackTop - 1))) ||                                   \
-        !IS_NUMBER((*(vm->stackTop - 2)))) {                                   \
-      printf("Operands must be numbers.\n");                                   \
-      return INTERPRET_RUNTIME_ERROR;                                          \
-    }                                                                          \
-    double b = AS_NUMBER(pop(vm));                                             \
-    double a = AS_NUMBER(pop(vm));                                             \
-    push(vm, valueType(a op b));                                               \
-  } while (false)
+#define BINARY_OP(valueType, op) \
+    do { \
+      if (!IS_NUMBER(peek(vm, 0)) || !IS_NUMBER(peek(vm, 1))) { \
+        runtimeError(vm, "Operands must be numbers."); \
+        return INTERPRET_RUNTIME_ERROR; \
+      } \
+      double b = AS_NUMBER(pop(vm)); \
+      double a = AS_NUMBER(pop(vm)); \
+      push(vm, valueType(a op b)); \
+    } while (false)
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
     printf("          ");
-    for (Value *slot = vm->stack; slot < vm->stackTop; slot++) {
+    for (Value* slot = vm->stack; slot < vm->stackTop; slot++) {
       printf("[ ");
       printValue(*slot);
       printf(" ]");
@@ -47,57 +121,52 @@ static InterpretResult run(VM *vm) {
     disassembleInstruction(vm->chunk, (int)(vm->ip - vm->chunk->code));
 #endif
 
-    u8 instruction;
+    uint8_t instruction;
     switch (instruction = READ_BYTE()) {
-    case OP_CONSTANT: {
-      Value constant = READ_CONSTANT();
-      push(vm, constant);
-      break;
-    }
-    case OP_TRUE:
-      push(vm, BOOL_VAL(true));
-      break;
-    case OP_FALSE:
-      push(vm, BOOL_VAL(false));
-      break;
-    case OP_NULL:
-      push(vm, NULL_VAL);
-      break;
-    case OP_POP:
-      pop(vm);
-      break;
-    case OP_ADD:
-      BINARY_OP(NUMBER_VAL, +);
-      break;
-    case OP_SUBTRACT:
-      BINARY_OP(NUMBER_VAL, -);
-      break;
-    case OP_MULTIPLY:
-      BINARY_OP(NUMBER_VAL, *);
-      break;
-    case OP_DIVIDE:
-      BINARY_OP(NUMBER_VAL, /);
-      break;
-    case OP_NOT:
-      push(vm, BOOL_VAL(IS_NULL(*(vm->stackTop - 1)) ||
-                        (IS_BOOL(*(vm->stackTop - 1)) &&
-                         !AS_BOOL(*(vm->stackTop - 1)))));
-      break;
-    case OP_NEGATE:
-      if (!IS_NUMBER(*(vm->stackTop - 1))) {
-        printf("Operand must be a number.\n");
-        return INTERPRET_RUNTIME_ERROR;
+      case OP_CONSTANT: {
+        Value constant = READ_CONSTANT();
+        push(vm, constant);
+        break;
       }
-      push(vm, NUMBER_VAL(-AS_NUMBER(pop(vm))));
-      break;
-    case OP_PRINT: {
-      printValue(pop(vm));
-      printf("\n");
-      break;
-    }
-    case OP_RETURN: {
-      return INTERPRET_OK;
-    }
+      case OP_TRUE:  push(vm, BOOL_VAL(true)); break;
+      case OP_FALSE: push(vm, BOOL_VAL(false)); break;
+      case OP_POP:   pop(vm); break;
+      case OP_NULL:  push(vm, NULL_VAL); break; 
+
+      case OP_ADD: {
+        if (IS_STRING(peek(vm, 0)) && IS_STRING(peek(vm, 1))) {
+          concatenate(vm);
+        } else if (IS_NUMBER(peek(vm, 0)) && IS_NUMBER(peek(vm, 1))) {
+          double b = AS_NUMBER(pop(vm));
+          double a = AS_NUMBER(pop(vm));
+          push(vm, NUMBER_VAL(a + b));
+        } else {
+          runtimeError(vm, "Operands must be two numbers or two strings.");
+          return INTERPRET_RUNTIME_ERROR;
+        }
+        break;
+      }
+      case OP_SUBTRACT: BINARY_OP(NUMBER_VAL, -); break;
+      case OP_MULTIPLY: BINARY_OP(NUMBER_VAL, *); break;
+      case OP_DIVIDE:   BINARY_OP(NUMBER_VAL, /); break;
+      case OP_NOT:
+        push(vm, BOOL_VAL(isFalsey(pop(vm))));
+        break;
+      case OP_NEGATE:
+        if (!IS_NUMBER(peek(vm, 0))) {
+          runtimeError(vm, "Operand must be a number.");
+          return INTERPRET_RUNTIME_ERROR;
+        }
+        push(vm, NUMBER_VAL(-AS_NUMBER(pop(vm))));
+        break;
+      case OP_PRINT: {
+        printValue(pop(vm));
+        printf("\n");
+        break;
+      }
+      case OP_RETURN: {
+        return INTERPRET_OK;
+      }
     }
   }
 
@@ -106,8 +175,22 @@ static InterpretResult run(VM *vm) {
 #undef BINARY_OP
 }
 
-InterpretResult interpret(VM *vm, Chunk *chunk) {
-  vm->chunk = chunk;
+// Compile and Run
+InterpretResult interpret(VM* vm, const char* source) {
+  Chunk chunk;
+  initChunk(&chunk);
+
+  if (!compile(source, &chunk)) {
+    freeChunk(&chunk);
+    return INTERPRET_COMPILE_ERROR;
+  }
+
+  vm->chunk = &chunk;
   vm->ip = vm->chunk->code;
-  return run(vm);
+
+  InterpretResult result = run(vm);
+
+  freeChunk(&chunk);
+  return result;
 }
+
