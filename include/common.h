f385fee1f69a4ab57b678914f70e6f086b12ac97
ProgrammerKR <programmerkr.123@gmail.com>
2026-01-07 16:30:07 +0530

    feat: Create common.h for foundational ProXPL types, macros, and forward declarations.

--- include/common.h
+++ include/common.h
@@ -91,8 +91,8 @@ typedef Value (*NativeFn)(int argCount, Value *args);
 #endif
 
 // Debugging
-#define DEBUG_PRINT_CODE
-#define DEBUG_TRACE_EXECUTION
+// #define DEBUG_PRINT_CODE
+// #define DEBUG_TRACE_EXECUTION
 // #define DEBUG_PRINT_CODE
 
 #endif // PROX_COMMON_H
