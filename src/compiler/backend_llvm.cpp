4218ad11175ac2928801cf02ed1e808f981501ea
ProgrammerKR <programmerkr.123@gmail.com>
2026-03-10 11:54:18 +0530

    Add PXCF configuration parser and fix macOS LLVM 19 build

--- src/compiler/backend_llvm.cpp
+++ src/compiler/backend_llvm.cpp
@@ -2,6 +2,7 @@
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
 #include <llvm/IR/Verifier.h>
+#include <llvm/IR/Intrinsics.h>
 #include <llvm/Support/raw_ostream.h>
 #include <vector>
 #include <map>
