fe2f234d8ce2b9680b6ecb35f27ec4f9ca42e222
ProgrammerKR <programmerkr.123@gmail.com>
2026-07-30 20:07:15 +0530

    Update src/runtime/chunk.c for v1.4.0

--- src/runtime/chunk.c
+++ src/runtime/chunk.c
@@ -15,12 +15,16 @@ void initChunk(Chunk *chunk) {
   chunk->code = NULL;
   chunk->lines = NULL;
   initValueArray(&chunk->constants);
+  chunk->exceptionHandlers.handlers = NULL;
+  chunk->exceptionHandlers.count = 0;
+  chunk->exceptionHandlers.capacity = 0;
 }
 
 void freeChunk(Chunk *chunk) {
   FREE_ARRAY(u8, chunk->code, chunk->capacity);
   FREE_ARRAY(int, chunk->lines, chunk->capacity);
   freeValueArray(&chunk->constants);
+  FREE_ARRAY(ExceptionHandler, chunk->exceptionHandlers.handlers, chunk->exceptionHandlers.capacity);
   initChunk(chunk);
 }
 
@@ -40,4 +44,16 @@ void writeChunk(Chunk *chunk, u8 byte, int line) {
 int addConstant(Chunk *chunk, Value value) {
   writeValueArray(&chunk->constants, value);
   return chunk->constants.count - 1;
+}
+
+void addExceptionHandler(Chunk *chunk, size_t start, size_t end, size_t handler) {
+  if (chunk->exceptionHandlers.capacity < chunk->exceptionHandlers.count + 1) {
+    int oldCapacity = chunk->exceptionHandlers.capacity;
+    chunk->exceptionHandlers.capacity = GROW_CAPACITY(oldCapacity);
+    chunk->exceptionHandlers.handlers = GROW_ARRAY(ExceptionHandler, chunk->exceptionHandlers.handlers, oldCapacity, chunk->exceptionHandlers.capacity);
+  }
+  chunk->exceptionHandlers.handlers[chunk->exceptionHandlers.count].start_ip = start;
+  chunk->exceptionHandlers.handlers[chunk->exceptionHandlers.count].end_ip = end;
+  chunk->exceptionHandlers.handlers[chunk->exceptionHandlers.count].handler_ip = handler;
+  chunk->exceptionHandlers.count++;
 }
